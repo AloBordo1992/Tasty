aba68658be02777b03c3a85fe2518c0f8404c7c8
AloBordo1992 <122152325+AloBordo1992@users.noreply.github.com>
2023-12-19 22:44:49 +0100

    to edd favicon, fonts,html header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/favorites">Favorites</NavLink>
+            </li>
+          </ul>
+          <NavLink to="/">
+            tasty<span>treats.</span>
+          </NavLink>
+          <button>*</button>
+        </nav>
+        <button>*</button>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
